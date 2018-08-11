b0bab58899e956d75c003750cc589e8dc7a26bda
liyuliang <liyuliang2013@gmail.com>
2018-08-12 00:44:42 +0800

    add elasticsearch model factory

---
+++ elasticsearch/factory.go
@@ -0,0 +1,20 @@
+package elasticsearch
+
+import (
+	"strings"
+	"errors"
+)
+
+func Get(modelName string) (model, error) {
+
+	l := List()
+
+	for name, creator := range l {
+
+		if strings.ToLower(name) == strings.ToLower(modelName) {
+			m := creator()
+			return m, nil
+		}
+	}
+	return nil, errors.New("can not get the Model by name :" + modelName)
+}
