9df2d39fa1d1028626e843ab4bc51827909c598f
糖叔 <liyuliang2013@gmail.com>
2018-09-02 20:59:21 +0800

    add protobuf list factory

---
+++ protobuf/list.go
@@ -0,0 +1,29 @@
+package protobuf
+
+type modelCreator func() model
+
+var creatorList []modelCreator
+
+func Register(method modelCreator) {
+
+	creatorList = append(creatorList, method)
+}
+
+type modelList map[string]modelCreator
+
+var _list modelList
+
+func List() modelList {
+
+	if len(_list) != len(creatorList) {
+
+		_list = make(map[string]modelCreator)
+
+		for _, creator := range creatorList {
+			m := creator()
+			_list[m.Name()] = creator
+		}
+	}
+
+	return _list
+}
---
+++ protobuf/model.go
@@ -0,0 +1,5 @@
+package protobuf
+
+type model interface {
+	Name() string
+}
