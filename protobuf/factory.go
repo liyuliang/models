c0907d27315b893b0f6d795095b3cb9d0e8dd96d
糖叔 <liyuliang2013@gmail.com>
2018-09-18 17:48:18 +0800

    add proto2 translate value methods

--- protobuf/factory.go
+++ protobuf/factory.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 	"errors"
 	"github.com/golang/protobuf/proto"
+	"time"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
 )
 
 func Get(modelName string) (Model, error) {
@@ -25,5 +28,46 @@ func Marshal(model proto.Message) ([]byte, error) {
 }
 
 func Unmarshal(buf []byte, model proto.Message) error {
+
 	return proto.Unmarshal(buf, model)
+}
+
+func Uint64(v uint64) *uint64 {
+	return proto.Uint64(v)
+}
+
+func Uint32(v uint32) *uint32 {
+	return proto.Uint32(v)
+}
+
+func Int(v int) *int32 {
+	return proto.Int(v)
+}
+
+func Int32(v int32) *int32 {
+	return proto.Int32(v)
+}
+
+func Int64(v int64) *int64 {
+	return proto.Int64(v)
+}
+
+func Float32(v float32) *float32 {
+	return proto.Float32(v)
+}
+
+func Float64(v float64) *float64 {
+	return proto.Float64(v)
+}
+
+func String(v string) *string {
+	return proto.String(v)
+}
+
+func Bool(v bool) *bool {
+	return proto.Bool(v)
+}
+
+func Timestamp(v time.Time) (*timestamp.Timestamp, error) {
+	return ptypes.TimestampProto(v)
 }
