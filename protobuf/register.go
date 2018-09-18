0bfefb841df0dbd2a4bc2a4bb1bbb53f210407d0
糖叔 <liyuliang2013@gmail.com>
2018-09-18 16:26:15 +0800

    shorten protobuf file path

--- protobuf/register.go
+++ protobuf/register.go
@@ -1,27 +1,25 @@
 package protobuf
 
-import "models/protobuf/task"
-
 func init() {
 	_list = make(map[string]modelCreator)
 
 	_list["parser_manhua_page"] = func() Model {
-		return new(task.ParserManhuaPage)
+		return new(ParserManhuaPage)
 	}
 
 	_list["saver_manhua_site"] = func() Model {
-		return new(task.SaverManhuaSite)
+		return new(SaverManhuaSite)
 	}
 
 	_list["saver_manhua_chapter"] = func() Model {
-		return new(task.SaverManhuaChapter)
+		return new(SaverManhuaChapter)
 	}
 
 	_list["saver_manhua_image"] = func() Model {
-		return new(task.SaverManhuaImage)
+		return new(SaverManhuaImage)
 	}
 
 	_list["indexer_manhua_chapter"] = func() Model {
-		return new(task.IndexManhuaChapter)
+		return new(IndexManhuaChapter)
 	}
 }
