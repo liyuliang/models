ac68cd27cd4962cce8e4f641380126a99f01294f
糖叔 <liyuliang2013@gmail.com>
2018-09-19 16:32:26 +0800

    add method to check model empty data

--- protobuf/model.go
+++ protobuf/model.go
@@ -1,9 +1,40 @@
 package protobuf
 
+import (
+	"strings"
+	"errors"
+)
+
 type Model interface {
 	GetId() uint64
 	Reset()
 	String() string
 	ProtoMessage()
 	Descriptor() ([]byte, []int)
+}
+
+func CheckModel(pfModel Model, data []byte) (err error) {
+
+	err = Unmarshal(data, pfModel)
+
+	s1 := pfModel.String()
+	pfModel.Reset()
+	s2 := pfModel.String()
+	if s1 == s2 {
+		err = errors.New("Empty Data In Protobuf Model. ")
+	}
+	return err
+}
+
+func IsModelNameExist(modelName string) bool {
+
+	exist := false
+	for name, _ := range List() {
+
+		if strings.ToLower(modelName) == strings.ToLower(name) {
+			exist = true
+			break
+		}
+	}
+	return exist
 }
